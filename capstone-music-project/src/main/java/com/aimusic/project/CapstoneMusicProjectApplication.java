82fc4ba1b14d752cda972f5ae100ba7f644a2725
trapsik-admin <hamjang99@gmail.com>
2025-09-24 15:09:21 +0900

    first commit

---
+++ capstone-music-project/src/main/java/com/aimusic/project/CapstoneMusicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aimusic.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneMusicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneMusicProjectApplication.class, args);
+	}
+
+}
---
+++ capstone-music-project/src/test/java/com/aimusic/project/CapstoneMusicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimusic.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneMusicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
