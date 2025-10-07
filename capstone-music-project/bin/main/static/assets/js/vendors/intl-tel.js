f049f164e3d1f0f29c57b55ae2ff4f0ff5e7633a
sehui <sehuiii0131@naver.com>
2025-10-07 16:32:11 +0900

    :wrench: gitignore 파일 수정

--- capstone-music-project/bin/main/static/assets/js/vendors/intl-tel.js
+++
@@ -1,1 +0,0 @@
-const input=document.querySelector("#phone");window.intlTelInput(input,{initialCountry:"in",strictMode:!0,utilsScript:"https://cdn.jsdelivr.net/npm/intl-tel-input@23.0.11/build/js/utils.js"});
