e90fc8022dabff4f0e4d3e59ffa7047755561686
sehui <sehuiii0131@naver.com>
2025-10-07 14:03:13 +0900

    :sparkles: /Feeling 엔드포인트 JSON 수신 및 기능 활성화

--- capstone-music-project/src/main/java/com/aimusic/project/controller/LandingApiController.java
+++ capstone-music-project/src/main/java/com/aimusic/project/controller/LandingApiController.java
@@ -2,6 +2,10 @@ package com.aimusic.project.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.*;
 
 import com.aimusic.project.service.LandingService;
@@ -11,16 +15,18 @@ import com.aimusic.project.service.LandingService;
 @RestController
 @RequestMapping("/api")
 public class LandingApiController {
-	private final LandingService LandingService;
+	private final LandingService landingService;
 	
 	// 사용자 감정 정보 수신 API
 	@PostMapping("/feeling")
-	public String receiveFeeling(@RequestParam("feeling") String feeling) {
-	    log.info("userFeeling = {}", feeling);
+	public Mono<String> receiveFeeling(@RequestBody Map<String,String> payload) {
+	    String feeling = payload.get("feeling");
+		log.info("userFeeling = {}", feeling);
 	    // TODO : AI 연동 시 서비스 호출 추가 예정
-	    return "사용자 감정: " + feeling;
+	    // return "사용자 감정: " + feeling;
+	    
 	    // 현재 주석코드: AI API base-URL 변경 시 주석 제거
-	    // return LandingService.forwardToAi(feeling);	// AI API 호출
+	    return landingService.forwardToAi(feeling);	// AI API 호출
 	}
 
 }
