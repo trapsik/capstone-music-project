56c57ccd0bd45ecc29a212e8266ed1c77d3b990d
sehui <sehuiii0131@naver.com>
2025-10-07 14:47:28 +0900

    :bug: AiResonpseDto에 NoArgsConstructor 추가 -> JSON 변환 오류 해결

--- capstone-music-project/src/main/java/com/aimusic/project/dto/AiResponseDto.java
+++ capstone-music-project/src/main/java/com/aimusic/project/dto/AiResponseDto.java
@@ -2,9 +2,11 @@ package com.aimusic.project.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor  // 매개변수가 없는 기본 생성자
+@AllArgsConstructor // 모든 필드를 매개변수로 받는 생성자
 public class AiResponseDto {
 	// 감정 분석 결과 필드 (성공)
 	private String emotion;
